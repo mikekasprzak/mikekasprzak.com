a0a8812dcf46f4f48994a44ee1fe308fd26d9deb
Mike Kasprzak <mikekasprzak@gmail.com>
2018-01-09 05:16:32 -0500

    added contact

--- src/bar/bar.js
+++ src/bar/bar.js
@@ -5,12 +5,14 @@ import UIButton							from 'ui/button/button';
 export default class Bar extends Component {
 	render( props ) {
 		let Hash = window.location.hash;
+		let Mail = atob('bWlrZUBzeWtocm9uaWNzLmNvbQ==');
 
 		return (
 			<div class="bar">
 				<UIButton class={cN((Hash == '#!/') ? 'active' : '')} href="#!/"><UIIcon src="home" /></UIButton>
 				<UIButton class={cN((Hash.indexOf('#!/project') == 0) ? 'active' : '')} href="#!/project"><UIIcon src="todo" /><span>Projects</span></UIButton>
 				<UIButton href="//mikekasprzak.com/Resume-MikeKasprzak.pdf"><UIIcon src="pdf" /><span>Resume</span></UIButton>
+				<UIButton href={"mailto:"+Mail+"?subject=mikekasprzak.com"} target="_blank"><UIIcon src="mail" /><span>Contact</span></UIButton>
 			</div>
 		);
 	}
