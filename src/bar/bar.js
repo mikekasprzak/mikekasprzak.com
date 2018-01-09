d768c02c4f3bce32d686d5fa7fa65aeab57670c7
Mike Kasprzak <mikekasprzak@gmail.com>
2018-01-09 05:44:52 -0500

    bar tweak

--- src/bar/bar.js
+++ src/bar/bar.js
@@ -10,7 +10,7 @@ export default class Bar extends Component {
 		return (
 			<div class="bar">
 				<UIButton class={cN((Hash == '#!/') ? 'active' : '')} href="#!/"><UIIcon src="home" /></UIButton>
-				<UIButton class={cN((Hash == '#!/about') ? 'active' : '')} href="#!/about"><UIIcon src="info" /><span>About</span></UIButton>
+				<UIButton class={cN((Hash.indexOf('#!/about') == 0) ? 'active' : '')} href="#!/about"><UIIcon src="info" /><span>About</span></UIButton>
 				<UIButton class={cN((Hash.indexOf('#!/project') == 0) ? 'active' : '')} href="#!/project"><UIIcon src="todo" /><span>Projects</span></UIButton>
 				<UIButton href="//blog.toonormal.com"><UIIcon src="article" /></UIButton>
 				<UIButton href="//mikekasprzak.com/Resume-MikeKasprzak.pdf"><UIIcon src="pdf" /><span>Resume</span></UIButton>
