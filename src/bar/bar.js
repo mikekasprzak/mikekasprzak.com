6fb7709168550c812aa52e3301a7536d290b02c4
Mike Kasprzak <mikekasprzak@gmail.com>
2018-01-09 05:27:01 -0500

    added twitter and shorter mail

--- src/bar/bar.js
+++ src/bar/bar.js
@@ -12,7 +12,9 @@ export default class Bar extends Component {
 				<UIButton class={cN((Hash == '#!/') ? 'active' : '')} href="#!/"><UIIcon src="home" /></UIButton>
 				<UIButton class={cN((Hash.indexOf('#!/project') == 0) ? 'active' : '')} href="#!/project"><UIIcon src="todo" /><span>Projects</span></UIButton>
 				<UIButton href="//mikekasprzak.com/Resume-MikeKasprzak.pdf"><UIIcon src="pdf" /><span>Resume</span></UIButton>
-				<UIButton href={"mailto:"+Mail+"?subject=mikekasprzak.com"} target="_blank"><UIIcon src="mail" /><span>Contact</span></UIButton>
+				<UIButton href="https://github.com/mikekasprzak" target="_blank"><UIIcon src="github" /></UIButton>
+				<UIButton href="https://twitter.com/mikekasprzak" target="_blank"><UIIcon src="twitter" /></UIButton>
+				<UIButton href={"mailto:"+Mail+"?subject=mikekasprzak.com"} target="_blank"><UIIcon src="mail" /></UIButton>
 			</div>
 		);
 	}
